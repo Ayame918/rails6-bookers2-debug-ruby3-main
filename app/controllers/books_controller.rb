8ede22a84588ff5f1e9800499d26e83b286796cb
EC2 Default User <ec2-user@ip-172-31-8-207.ap-northeast-1.compute.internal>
2023-11-04 13:38:10 +0000

    応用課題２　テスト後エラー８

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,27 +2,34 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
-    @user = User.find(params[:id])
+    @user = @book.user
   end
 
   def index
     @books = Book.all
     @book = Book.new
+    @user = current_user
   end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
+      @user = current_user
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
       @books = Book.all
-      render 'new'
+      @user = current_user
+      render 'show'
     end
   end
 
   def edit
     @book = Book.find(params[:id])
+    @user = @book.user
+    if @user != current_user
+      redirect_to books_path
+    end
   end
 
   def update
@@ -30,14 +37,15 @@ class BooksController < ApplicationController
     if @book.update(book_params)
       redirect_to book_path(@book), notice: "You have updated book successfully."
     else
+      @user = @book.user
       render "edit"
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
-    redirect_to books_path
+    @book.destroy
+    redirect_to books_path(id: current_user)
   end
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   before_action :ensure_correct_user, only: [:update]
+  #before_action :authenticate_user!
+  #before_action :authorize_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -14,12 +16,15 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    if @user !=current_user
+      redirect_to user_path(current_user.id)
+    end
   end
 
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to users_path(@user), notice: "You have updated user successfully."
+      redirect_to users_path(current_user), notice: "You have updated user successfully."
     else
       render "edit"
     end
