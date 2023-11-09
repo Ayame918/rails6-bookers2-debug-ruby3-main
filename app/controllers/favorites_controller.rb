027567f9ba1484caf8200b9f39d13f3ee8c45835
EC2 Default User <ec2-user@ip-172-31-8-207.ap-northeast-1.compute.internal>
2023-11-09 14:26:50 +0000

    応用課題６ 非同期通信化[Finish]

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,14 +4,16 @@ class FavoritesController < ApplicationController
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: book.id)
     favorite.save
-    #redirect_to request.referer#リクエストを送信した元のURLを表示する
+    redirect_to request.referer#リクエストを送信した元のURLを表示する
+    #render 'replace_btn'
   end
   
   def destroy
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
-    #redirect_to request.referer
+    redirect_to request.referer
+    #render 'replace_btn'
   end
   
 end
